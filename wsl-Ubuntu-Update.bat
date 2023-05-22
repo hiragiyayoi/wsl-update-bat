6e8aa117c190c97820607b061d9371a0c5a36b84
hiragiyayoi <84854644+hiragiyayoi@users.noreply.github.com>
2023-05-22 14:20:28 +0900

    Update wsl-Ubuntu-Update.bat

--- wsl-Ubuntu-Update.bat
+++ wsl-Ubuntu-Update.bat
@@ -3,7 +3,7 @@ powershell -Command "wsl --update"
 wsl sudo apt update
 wsl sudo dpkg --configure -a
 wsl sudo apt upgrade -y
-wsl sudo apt install gcc
+wsl sudo apt install gcc -y
 
 echo XY | choice
 
